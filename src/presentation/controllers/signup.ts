e37c6e91f83500e2356175458272f012041939f3
Rafael <rafaeldesenv.net@gmail.com>
2022-11-11 01:59:05 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,13 +5,13 @@ import { badRequest } from "../helpers/http-helper";
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
 
     for (const field of requiredFields) {
       if(!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
-      }
-    }
+      } 
+   } 
 
     return {
       statusCode: 200,
